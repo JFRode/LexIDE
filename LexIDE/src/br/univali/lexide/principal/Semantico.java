0ef5a6f778f3272382cd9dc389af98de97067a06
5108250 <5108250@U705012.univali.br>
2016-05-03 22:07:08 -0300

    Reconhecimento de Token adicionado. Caso 2 de tratamento adicionado..

--- LexIDE/src/br/univali/lexide/principal/Semantico.java
+++ LexIDE/src/br/univali/lexide/principal/Semantico.java
@@ -93,7 +93,6 @@ public class Semantico implements Constants {
                         podeInserir = false;
                     }
                 }
-
             }
 
             if (podeInserir) {
