6e55393521b82ace2ed76d570dcd9e5551d6d907
5108250 <5108250@U705012.univali.br>
2016-05-03 21:57:52 -0300

    Reconhecimento de Token adicionado. Caso 2 de tratamento adicionado.

--- LexIDE/src/br/univali/lexide/principal/Semantico.java
+++ LexIDE/src/br/univali/lexide/principal/Semantico.java
@@ -6,90 +6,114 @@ import java.util.List;
 import java.util.Stack;
 
 public class Semantico implements Constants {
-    List<Tupla> tabela = new ArrayList();
-    Stack<String> pilha = new Stack();
-    Tupla temp = new Tupla();
-    
-    public void executeAction(int action, Token token)	throws SemanticError {
+
+    List<Tupla> tabela;
+    Stack<String> pilha;
+    Tupla temp;
+
+    public Semantico() {
+        tabela = new ArrayList();
+        pilha = new Stack();
+        temp = new Tupla();
+    }
+
+    public void executeAction(int action, Token token) throws SemanticError {
+
         switch (action) {
             case 1: // name
+
                 temp.setNome(token.getLexeme());
-                insereTabela();
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 2: // type
                 temp.setTipo(token.getLexeme());
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 3: // inicialized
                 temp.setInicializado(true);
-                insereTabela();
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 4: // used
                 temp.setUsado(true);
-                insereTabela();
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 5: // scope
                 pilha.push(token.getLexeme());
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 6: // param
                 temp.setParametro(true);
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 7: // position
                 temp.setPos(token.getPosition());
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 8: // vector
                 temp.setVetor(true);
-                insereTabela();
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 9: // matrix
                 temp.setMatriz(true);
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 10: // ref
                 temp.setRef(true);
-                insereTabela();
-                System.out.println("Ação #"+action+", Token: "+token);
+                System.out.println("Ação #" + action + ", Token: " + token);
                 break;
             case 11: // func
                 temp.setFuncao(true);
+                System.out.println("Ação #" + action + ", Token: " + token);
+                break;
+            case 12: // ;
                 insereTabela();
-                System.out.println("Ação #"+action+", Token: "+token);
+                temp = new Tupla();
+                System.out.println("Ação #" + action + ", Token: " + token);
+                imprimeTabela();
                 break;
         }
     }
-    
+
     public void insereTabela() {
+        boolean podeInserir = true;
         temp.setEscopo(pilha.firstElement());
-        
         if (tabela.size() != 0) {
             for (Tupla t : tabela) {
-                if (temp.getNome().equals(t.getNome()) && temp.getEscopo().equals(t.getEscopo())) {
-                    System.out.println(t.getNome() + " Ja existe neste escopo!");
-                } else {
-                    tabela.add(temp);
-                    System.out.println("INSERIU");
+                // 2. declaracao no mesmo escopo
+                if (t.getNome().equals(temp.getNome()) && t.getEscopo().equals(temp.getEscopo())) {
+                    if (temp.getTipo() != null) {
+                        podeInserir = false;
+                    } else {
+                        System.out.println("oi");
+                        t.setInicializado(temp.isInicializado());
+                        t.setUsado(temp.isUsado());
+                        t.setParametro(temp.isParametro());
+                        t.setPos(temp.getPos());
+                        t.setRef(temp.isRef());
+                        podeInserir = false;
+                    }
                 }
+
             }
+
+            if (podeInserir) {
+                tabela.add(temp);
+                System.out.println("INSERIU");
+            }
+
         } else {
             tabela.add(temp);
             System.out.println("INSERIU");
         }
         imprimeTabela();
     }
-    
+
     public void imprimeTabela() {
         System.out.println("-------------------------------------------------------------------");
         System.out.println("NOME\tTIPO\tINIC\tUSADO\tESCOPO\tPARAM\tPOS\tVET\tMATRIZ\tREF\tFUNC");
         for (Tupla t : tabela) {
             System.out.print(t.getNome() + "\t");
-            System.out.print(t.getTipo()+ "\t");
+            System.out.print(t.getTipo() + "\t");
             System.out.print(t.isInicializado() + "\t");
             System.out.print(t.isUsado() + "\t");
             System.out.print(t.getEscopo() + "\t");
